a76b475390c1f75bc5170e646e2635bd339cec98
Elor-Itz <elor2200@gmail.com>
2025-03-08 13:40:15 +0200

    Firebase setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,10 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+
+    id("com.android.application")
+    // Add the Google services Gradle plugin
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -68,4 +72,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
 
-}
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:33.10.0"))
+
+    }
