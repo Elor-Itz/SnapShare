ee6ae5e49f0ba5d056cc86bf1bdb5888a1d6657f
Adica6 <adicahal@gmail.com>
2025-03-30 10:23:29 +0200

    added post into the AppDatabase

--- app/src/main/java/com/example/snapshare/data/local/AppDatabase.kt
+++ app/src/main/java/com/example/snapshare/data/local/AppDatabase.kt
@@ -5,10 +5,12 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.snapshare.data.model.User
+import com.example.snapshare.data.model.Post
 
-@Database(entities = [User::class], version = 2, exportSchema = false)
+@Database(entities = [User::class, Post::class], version = 2, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun userDao(): UserDao
+    abstract fun postDao(): PostDao
 
     companion object {
         @Volatile
